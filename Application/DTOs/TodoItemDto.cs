0fcee16cc5f07ebcf7f9ca097410c59a6fd787fd
Ahmet Kürşad YILDIZ <ahmetkursadyildiz@gmail.com>
2025-12-17 04:06:10 +0300

    Add DTOs

---
+++ Application/DTOs/TodoItemDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs
+{
+    public class TodoItemDto
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdateAt { get; set; }
+
+    }
+}
---
+++ Application/DTOs/UpdateTodoItemDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs
+{
+    public class UpdateTodoItemDto
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
