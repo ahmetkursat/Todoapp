1297fcd5a4f30f2ba8f2ed8fffe85c5d59c30e02
Ahmet Kürşad YILDIZ <ahmetkursadyildiz@gmail.com>
2025-12-17 03:29:29 +0300

    TodoException

---
+++ Core/Exceptions/TodoDomainException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Core.Exceptions
+{
+    public class TodoDomainException : Exception
+    {
+        public TodoDomainException()
+        {
+
+        }
+        public TodoDomainException(string? message) : base(message)
+        {
+
+        }
+        public TodoDomainException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+        protected TodoDomainException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
