fd232a05c38a6e110ae34a9384f5af7bb8dd63a3
Ahmet Kürşad YILDIZ <ahmetkursadyildiz@gmail.com>
2025-12-17 04:06:45 +0300

    Add ITodoService

---
+++ Application/Interfaces/ITodoService.cs
@@ -0,0 +1,20 @@
+﻿using Application.DTOs;
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces
+{
+    public interface ITodoService
+    {
+        Task<TodoItemDto?> GetByIdAsync(int id);
+        Task<IEnumerable<TodoItemDto>> GetAllAsync();
+        Task<IEnumerable<TodoItemDto>> GetCompletedAsync();
+        Task<IEnumerable<TodoItemDto>> GetPendingAsync();
+        Task<TodoItemDto> CreateAsync(CreateTodoItemDto dto);
+        Task<TodoItemDto> UpdateAsync(UpdateTodoItemDto dto);
+        Task<bool> DeleteAsync(int id);
+      
+    }
+}
