cc20515b1c255850f1c74a444cd6eeeb06f1ebbd
Ahmet Kürşad YILDIZ <ahmetkursadyildiz@gmail.com>
2025-12-18 03:30:32 +0300

    Error and Exceptions Add

---
+++ Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Common.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public string ResourceName { get; }
+
+        public object ResourceId { get; }
+
+        public NotFoundException(string resourceName, object resourceId)
+           : base($"{resourceName} with id {resourceId} not found")
+        {
+            ResourceName = resourceName;
+            ResourceId = resourceId;
+        }
+    }
+}
---
+++ Application/Common/Exceptions/ValidationException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Common.Exceptions
+{
+    public class ValidationException :Exception
+    {
+        public IEnumerable<string> Errors { get;}
+
+        public ValidationException(IEnumerable<string> errors)
+            : base("One or more validation errors occurred")
+        {
+            Errors = errors;
+        }
+
+        public ValidationException(string error)
+            :base("Validation error occurred")
+        {
+            Errors = new[] { error };
+        }
+
+    }
+}
