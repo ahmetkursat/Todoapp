62ddee277012fd46497b40cda44e4358d08ca2e6
Ahmet Kürşad YILDIZ <ahmetkursadyildiz@gmail.com>
2025-12-20 20:04:16 +0300

    Mapping

--- Application/Mappings/MappingProfile.cs
+++ Application/Mappings/MappingProfile.cs
@@ -11,26 +11,7 @@ namespace Application.Mappings
     {
        public MappingProfile()
         {
-            // Entity -> DTO (Basit mapping)
             CreateMap<TodoItem, TodoItemDto>();
-
-            // CreateDTO -> Entity
-            CreateMap<CreateTodoItemDto, TodoItem>()
-                .ForMember(dest => dest.Id, opt => opt.Ignore())
-                .ForMember(dest => dest.IsCompleted, opt => opt.MapFrom(src => false))
-                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow));
-       
-
-            // UpdateDTO -> Entity (SADECE NULL OLMAYAN ALANLAR)
-            CreateMap<UpdateTodoItemDto, TodoItem>()
-                .ForMember(dest => dest.Id, opt => opt.Ignore())
-                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
-                .ForMember(dest => dest.IsCompleted, opt => opt.Ignore())
-               
-                // Title: Sadece null değilse map et
-                .ForMember(dest => dest.Title, opt => opt.Condition(src => src.Title != null))
-                // Description: Sadece null değilse map et
-                .ForMember(dest => dest.Description, opt => opt.Condition(src => src.Description != null));
         }
 
     }
