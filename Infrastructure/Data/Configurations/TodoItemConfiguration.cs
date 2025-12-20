355d6a5be3b05b420dbb34b7cb9763fd6ec47048
Ahmet Kürşad YILDIZ <ahmetkursadyildiz@gmail.com>
2025-12-20 20:08:27 +0300

    Infrastructure added

---
+++ Infrastructure/Data/Configurations/TodoItemConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data.Configurations
+{
+    public class TodoItemConfiguration : IEntityTypeConfiguration<TodoItem> 
+    {
+        public void Configure(EntityTypeBuilder<TodoItem> builder)
+        {
+            builder.ToTable("TodoItems");
+
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.Title)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(t => t.Description)
+                .HasMaxLength(500);
+
+            builder.Property(t => t.CreatedAt)
+                .HasDefaultValueSql("GETDATE()");
+
+            builder.HasIndex(t => t.IsCompleted)
+                .HasDatabaseName("IX_TodoItem_IsCompleted");
+
+        }
+    }
+}
---
+++ Infrastructure/Repositories/TodoRepository.cs
@@ -0,0 +1,31 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Repositories
+{
+    public class TodoRepository : Repository<TodoItem>, ITodoRepository
+    {
+        public TodoRepository(DbContext context, DbSet<TodoItem> dbset) : base(context, dbset)
+        {
+        }
+
+        public async Task <IEnumerable<TodoItem>> GetComletedAsync()
+        {
+            return await _context.Set<TodoItem>()
+                .AsNoTracking().Where(x => x.IsCompleted)
+             
+        }
+        public async Task<IEnumerable<TodoItem>> GetPendingAsync()
+        {
+            return await _context.Set<TodoItem>()
+                                 .AsNoTracking()
+                                 .Where(x => !x.IsCompleted) // Tamamlanmamışlar
+                                 .ToListAsync();
+        }
+    }
+}
